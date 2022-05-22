6d52c781a2c1cc340b767237512740f478fb08bf
alex.kim <alkkim@gmail.com>
2022-05-21 23:40:07 -0700

    changed question answers from object to an array instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,68 @@
-const questions = {
-    q1: [
-        "Which of the following is correct about features of JavaScript?",
-        "1. JavaScript is a lightweight, interpreted programming language.",
-        "2. JavaScript is designed for creating network-centric applications.",
-        "3. JavaScript is complementary to and integrated with Java.",
-        "4. All of the above.",
-    ],
-    q2: [
-        "What are variables used for in JavaScript Programs?",
-        "1. Storing numbers, dates, or other values",
-	    "2. Varying randomly",
-	    "3. Causing high-school algebra flashbacks",
-        "4. None of the above",
-    ],
-    q3: [
-        "The _______ method of an Array object adds and/or removes elements from an array.",
-        "1. Reverse",
-        "2. Shift",
-        "3. Slice",
-        "4. Splice",
-    ],
-    q4: [
-        "Which of the following type of variable is visible everywhere in your JavaScript code?",
-        "1. global variable",
-        "2. local variable",
-        "3. Both of the above",
-        "4. None of the above",
-    ],
-    q5: [
-        "String values must be enclosed within ____ when being assigned to variables",
-        "1. commas",
-		"2. curly brackets",
-		"3. parenthesis",
-        "4. quotes",
-    ]
-}
+const testQs = [
+    { 
+        question: "Which of the following is correct about features of JavaScript?",
+        answers: [
+            "JavaScript is a lightweight, interpreted programming language.",
+            "JavaScript is designed for creating network-centric applications.",
+            "JavaScript is complementary to and integrated with Java.",
+            "All of the above.",
+        ],
+        correct: "All of the above.",
+    },
+    {
+        question: "What are variables used for in JavaScript Programs?",
+        answers: [
+            "Storing numbers, dates, or other values",
+            "Used in calling a function",
+            "Varying randomly",
+            "None of the above",
+        ],
+        correct: "Storing numbers, dates, or other values",
+    },
+    {
+        question: "The _______ method of an Array object adds and/or removes elements from an array.",
+        answers: [ 
+            "reverse",
+            "shift",
+            "slice",
+            "splice",
+        ],
+        correct: "splice",
+    },
+    {
+        question: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+        answers: [
+            "global variable",
+            "local variable",
+            "Both of the above",
+            "None of the above",
+        ],
+        correct: "global variable",
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables",
+        answers: [
+            "commas",
+            "curly brackets",
+            "parenthesis",
+            "quotes",
+        ],
+        correct: "commas",
+    },
+]
 
-let arrLen = Object.keys(questions).length;
+var allQuestionsEl = document.getElementById("all-questions");
+var answersEl = document.getElementById("answers");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
 
-for (let i = 0; i < arrLen; i++) {
-    let n = Object.keys(questions)[i];
-    let x = (questions[n]);
-    for (let count = 0; count < x.length; count++) {
-        console.log(x[count]);
-    }
-}
+// function getQuestions() {
+//     for (let i = 0; i < testQs.length; i++) {
+//         var insertQuestionsEl = document.getElementById("questions");
+//         insertQuestionsEl.textContent = testQs[i].question;
+//     }
+// };
+
+// getQuestions();
+
+console.log(testQs[0].answers);
